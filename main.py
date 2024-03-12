ea7cbfcc788c08090bb7f4ab5ebc5b2bbb830da8
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-12 13:35:57 +0300

    up

--- main.py
+++ main.py
@@ -12,20 +12,22 @@ def main():
     name_file = 'id'
     Path(name_folder).mkdir(parents=True, exist_ok=True)
     for i in range(1, 11):
-
-        payload = {'id': str(i)}
-        response = requests.get('https://tululu.org/txt.php', params=payload)
-        response.raise_for_status()
-        check_for_redirect(response)
-
         try:
+            payload = {'id': str(i)}
+            response = requests.get('https://tululu.org/txt.php', params=payload)
+            response.raise_for_status()
+            check_for_redirect(response)
             filename = f'{name_folder}/{name_file}{str(i)}.txt'
             with open(filename, 'w') as file:
                 file.write(response.text)
             print('try', i)
+
         except HTTPError:
             print('except', i)
             continue
+
+
+
 
 if __name__ == '__main__':
     main()
