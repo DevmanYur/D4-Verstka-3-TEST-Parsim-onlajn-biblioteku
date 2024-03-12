7c112dc1489300f2a5633a0857b0ade65f2b0a97
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-12 11:57:18 +0300

    upd

--- main.py
+++ main.py
@@ -2,53 +2,29 @@ import requests
 from pathlib import Path
 from requests import HTTPError
 
-def get_book():
-    payload = {'id': '2'}
-    url = 'https://tululu.org/txt.php'
-    r = requests.get(url, params=payload)
-    print(r.history)
-
-    if r.history==[]:
-        print("ответ пришёл с запрошенной страницы")
-    else:
+def check_for_redirect(response):
+    if response.history!=[]:
         print("ответ пришёл с главной")
+        raise HTTPError
+
+def main():
+    name_folder = 'book'
+    name_file = 'id'
+    Path(name_folder).mkdir(parents=True, exist_ok=True)
+    for i in range(1, 11):
+        url = 'https://tululu.org/txt.php?id=' + str(i)
+        response = requests.get(url)
+        check_for_redirect(response)
+
+        try:
+            response.raise_for_status()
+            filename = name_folder + '/' + name_file + str(i) + '.txt'
+            with open(filename, 'w') as file:
+                file.write(response.text)
+            print('try', i)
+        except HTTPError:
+            print('except', i)
+            continue
 
 if __name__ == '__main__':
-    get_book()
-
-
-
-
-
-    # try :
-    #     if r.history==[]:
-    #         print("good")
-    #     else:
-    #         print("bed")
-    #
-    # except OSError:
-    #     print("bed2")
-        # Инструкция raise без выражения поднимет FileNotFoundError повторно.
-        # raise HTTPError
-
-    # payload = {'id': '1'}
-    # r = requests.get('https://tululu.org/txt.php', params=payload )
-    # print(r.url)
-    # print(r.history)
-
-
-    # name_folder = 'book'
-    # name_file = 'id'
-    # Path(name_folder).mkdir(parents=True, exist_ok=True)
-    #
-    # for i in range(1, 11):
-    #     url = 'https://tululu.org/txt.php?id=' + str(i)
-    #     response = requests.get(url)
-    #     response.raise_for_status()
-    #     filename = name_folder + '/' + name_file + str(i) + '.txt'
-    #     with open(filename, 'w') as file:
-    #         file.write(response.text)
-
-
-
-
+    main()
