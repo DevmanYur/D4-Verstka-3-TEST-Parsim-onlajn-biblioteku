47a4e4fedc65629544319bfb3dbab2c871a79768
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-14 11:55:30 +0300

    upd

--- main.py
+++ main.py
@@ -9,8 +9,34 @@ from urllib.parse import urljoin
 from urllib.parse import urlparse
 from urllib.parse import unquote
 
-def download_image (folder='images/'):
+
+def download_image(url, filename, folder='images/'):
+
+
+    # Создаем папку
     Path(folder).mkdir(parents=True, exist_ok=True)
+
+    # Ссылка на страницу
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # Суп страницы
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # Ищем картинку и вычисляем к ней путь
+    image_tag = soup.find(class_='bookimage').find('img')['src']
+    image_way = urljoin('https://tululu.org/', image_tag)
+    print(image_way)
+
+    # Ссылка на картинку
+    response = requests.get(image_way)
+    response.raise_for_status()
+
+    way = os.path.join(folder, sanitize_filename(filename))
+    filename_ = f'{way}.jpg'
+
+    with open(filename_, 'wb') as file:
+        file.write(response.content)
 
 
 def download_txt(url, filename, folder='books/'):
@@ -22,6 +48,7 @@ def download_txt(url, filename, folder='books/'):
     filename_ = f'{way}.txt'
     with open(filename_, 'w') as file:
         file.write(response.text)
+    print(filename_)
     return filename_
 
 
@@ -32,39 +59,42 @@ def check_for_redirect(response, number):
 
 def main():
 
+    url = 'https://tululu.org/'
+
     for i in range(1, 11):
         try:
             payload = {'id': str(i)}
-            response = requests.get('https://tululu.org/txt.php', params=payload)
+            response = requests.get(f'{url}txt.php', params=payload)
             response.raise_for_status()
-            url = response.url
+
 
             check_for_redirect(response, i)
 
-            url2 = f'https://tululu.org/b{i}'
-            response2 = requests.get(url2)
+            response2 = requests.get(f'{url}b{i}')
             response2.raise_for_status()
+
             soup = BeautifulSoup(response2.text, 'lxml')
-            title_tag = soup.find('h1')
-            title_text = title_tag.text
-            y = title_text.split('::')
-            filename = f'{i}. {y[0].strip()}'
+            tittle_tag = soup.find('h1').text.split('::')
+            tittle_name = f'{i}. {tittle_tag[0].strip()}'
 
-            img_tag = soup.find(class_='bookimage').find('img')['src']
-            x = urljoin('https://tululu.org/', img_tag)
-            print(filename)
-            print(x)
-
-            y = urlparse(x)
-            z = unquote(y)
-
-            
-
-            print(y.path)
+            download_txt(response.url, tittle_name)
 
 
-            download_txt(url, filename)
-            download_image()
+            download_image(response2.url, str(i))
+
+            image_tag = soup.find(class_='bookimage').find('img')['src']
+            image_way = urljoin('https://tululu.org/', image_tag)
+
+            print(tittle_name)
+            print('Сама страница', response2.url)
+            print('Cтраница на текст скачивания',response.url)
+            print('Путь до картинки',image_way)
+
+
+
+
+
+
 
         except HTTPError:
             continue
