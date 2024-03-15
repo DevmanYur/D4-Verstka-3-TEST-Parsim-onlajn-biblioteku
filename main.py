78d0dc437da689f3b5244222d1fca938eb8a317c
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-15 18:17:19 +0300

    up

--- main.py
+++ main.py
@@ -10,66 +10,27 @@ from urllib.parse import urlparse
 from urllib.parse import unquote
 
 
+def download_image(url, filename, folder='images/'):
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    image_tag = soup.find(class_='bookimage').find('img')['src']
+    image_way = urljoin('https://tululu.org/', image_tag)
+    print(image_way)
+
+    response_image_way = requests.get(image_way)
+    response_image_way.raise_for_status()
+
+    way = os.path.join(folder, sanitize_filename(str(filename)))
+    filename_ = f'{way}.jpg'
+
+    with open(filename_, 'wb') as file:
+        file.write(response_image_way.content)
 
 
-
-# def download_comm(url):
-#     url = 'https://tululu.org/b5'
-#
-#
-#     response = requests.get(url)
-#     response.raise_for_status()
-#
-#     soup = BeautifulSoup(response.text, 'lxml')
-#     comm = soup.find('span', class_='d_book').find_all('a')
-#     for x in comm:
-#         print(x.text)
-#
-#
-#
-# def download_comments(url):
-#     # Ссылка на страницу
-#     response = requests.get(url)
-#     response.raise_for_status()
-#
-#     # Суп страницы
-#     soup = BeautifulSoup(response.text, 'lxml')
-#
-#     # Ищем комментарий и вычисляем к ней путь
-#     comm_tag = soup.find_all(class_='texts')
-#     for x in comm_tag:
-#         print(x.find(class_='black').text)
-#
-#
-# def download_image(url, filename, folder='images/'):
-#
-#
-#     # Создаем папку
-#     Path(folder).mkdir(parents=True, exist_ok=True)
-#
-#     # Ссылка на страницу
-#     response = requests.get(url)
-#     response.raise_for_status()
-#
-#     # Суп страницы
-#     soup = BeautifulSoup(response.text, 'lxml')
-#
-#     # Ищем картинку и вычисляем к ней путь
-#     image_tag = soup.find(class_='bookimage').find('img')['src']
-#     image_way = urljoin('https://tululu.org/', image_tag)
-#     print(image_way)
-#
-#     # Ссылка на картинку
-#     response = requests.get(image_way)
-#     response.raise_for_status()
-#
-#     way = os.path.join(folder, sanitize_filename(filename))
-#     filename_ = f'{way}.jpg'
-#
-#     with open(filename_, 'wb') as file:
-#         file.write(response.content)
-#
-#
 def download_txt(url, filename, folder='books/'):
 
     Path(folder).mkdir(parents=True, exist_ok=True)
@@ -132,39 +93,15 @@ def main():
             # Страница книги xml
             response_book_page = requests.get(f'{url}b{filename}')
             response_book_page.raise_for_status()
+            print(response_book_page.url)
             soup = BeautifulSoup(response_book_page.text, 'lxml')
 
             # Запуск функции на парсинг
             parse_book_page(soup)
 
-            download_txt(url, filename, folder='books/')
-
-
-
-
-            #
-            #
-            # download_txt(response.url, tittle_name)
-            #
-            #
-            # download_image(response2.url, str(i))
-            #
-            # download_comments(response2.url)
-            #
-            #
-            #
-            # image_tag = soup.find(class_='bookimage').find('img')['src']
-            # image_way = urljoin('https://tululu.org/', image_tag)
-            #
-            # print(tittle_name)
-            # print('Сама страница', response2.url)
-            # print('Cтраница на текст скачивания',response.url)
-            # print('Путь до картинки',image_way)
-
-
-
-
-
+            # Запуск функции на скачивание текста
+            download_txt(response.url, filename)
+            download_image(response_book_page.url, filename)
 
 
         except HTTPError:
