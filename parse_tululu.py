20527f62ed0aad429da690579959c6de30e7c3c2
DevmanYur <devmanyur@gmail.com>
2024-03-17 16:52:43 +0300

    up

--- parse_tululu.py
+++ parse_tululu.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 import requests
 from pathlib import Path
@@ -6,12 +7,9 @@ from requests import HTTPError
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from urllib.parse import urlparse
-from urllib.parse import unquote
 
 
 def download_image(url, filename, folder='images/'):
-
     Path(folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
@@ -19,7 +17,6 @@ def download_image(url, filename, folder='images/'):
 
     image_tag = soup.find(class_='bookimage').find('img')['src']
     image_way = urljoin('https://tululu.org/', image_tag)
-    print(image_way)
 
     response_image_way = requests.get(image_way)
     response_image_way.raise_for_status()
@@ -32,7 +29,6 @@ def download_image(url, filename, folder='images/'):
 
 
 def download_txt(url, filename, folder='books/'):
-
     Path(folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
@@ -43,22 +39,17 @@ def download_txt(url, filename, folder='books/'):
     return way_filename
 
 
-
 def parse_book_page(soup):
-
-    # Заголовок
     tittle_tag = soup.find('h1').text.split('::')
     tittle = f'{tittle_tag[0].strip()}'
     print(f'Заголовок: {tittle}')
 
-    # Жанр
     genre_tag = soup.find('span', class_='d_book').find_all('a')
-    genres =[]
-    for genre in genre_tag :
+    genres = []
+    for genre in genre_tag:
         genres.append(genre.text)
     print(f'Жанр: {genres}')
 
-    # Комментарии
     comments_tag = soup.find_all(class_='texts')
     comments = []
     for comment in comments_tag:
@@ -66,40 +57,36 @@ def parse_book_page(soup):
     print(f'Комментарии: {comments}')
 
 
-
-
-
 def check_for_redirect(response):
-    if response.history!=[]:
+    if response.history:
         raise HTTPError
 
+
 def main():
+    parser = argparse.ArgumentParser(
+        description='Скачивание заданных страниц'
+    )
+    parser.add_argument('start_id', help='Страница с', type=int)
+    parser.add_argument('end_id', help='Страница по', type=int)
+    args = parser.parse_args()
 
     url = 'https://tululu.org/'
-    list = [5]
 
-    for filename in list:
+    for filename in range(args.start_id, args.end_id+1):
         try:
 
-            # Страница загрузки txt файла
             payload = {'id': str(filename)}
             response = requests.get(f'{url}txt.php', params=payload)
             response.raise_for_status()
-            print(response.url)
 
-            # Проверка на редирект
             check_for_redirect(response)
 
-            # Страница книги xml
             response_book_page = requests.get(f'{url}b{filename}')
             response_book_page.raise_for_status()
-            print(response_book_page.url)
             soup = BeautifulSoup(response_book_page.text, 'lxml')
 
-            # Запуск функции на парсинг
             parse_book_page(soup)
 
-            # Запуск функции на скачивание текста
             download_txt(response.url, filename)
             download_image(response_book_page.url, filename)
 
