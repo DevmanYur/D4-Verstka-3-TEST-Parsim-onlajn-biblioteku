4e0749767a47c82c17c691dfa3205541015f3c71
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-05-21 13:57:43 +0300

    Update parse_tululu.py

--- parse_tululu.py
+++ parse_tululu.py
@@ -50,78 +50,22 @@ def f1 ():
 
                 # Суп страницы
                 soup = BeautifulSoup(response_page.text, 'lxml')
+                tittle, author, comments, genres, image, image_tag = parse_book_page(soup)
 
 
-                # Заголовок
-                tittle, author = soup.find('h1').text.split('::')
-                sanitize_tittle = sanitize_filename(tittle.strip())
-
-
-                # Картинка
-                tag_image = soup.find(class_='bookimage').find('img')['src']
-                url_image = f'{url}{tag_image}'
+                url_image = f'{url}{image_tag}'
                 response_image = requests.get(url_image)
                 response_image.raise_for_status()
 
-                # Комментарии
-                comments_tag = soup.find_all(class_='texts')
-                comments = []
-                for comment in comments_tag:
-                    comments.append(comment.find(class_='black').text)
-
-
-                # Жанр
-                genre_tag = soup.find('span', class_='d_book').find_all('a')
-                genres = []
-                for genre in genre_tag:
-                    genres.append(genre.text)
 
 
 
 
 
 
-
-                name_file = f'{x+1}. {sanitize_tittle}'
-                print(name_file)
-                print('страница :', response_page.url)
-                print('картинка :', url_image)
-                print('тег картинка :', tag_image)
-                name_image = tag_image.split('/')[-1]
-                print(name_image)
-                print('комментарии :', comments)
-                print('жанр :', genres)
-                print()
-
-                folder_for_books = 'books'
-                Path(folder_for_books).mkdir(parents=True, exist_ok=True)
-                x = os.path.join(folder_for_books, name_file)
-                y = f'{x}.txt'
-                with open(y, 'w') as file:
-                     file.write(response_txt.text)
-
-
-                folder_for_images = 'images'
-                Path(folder_for_images).mkdir(parents=True, exist_ok=True)
-                z = os.path.join('images', name_image)
-                q = f'{z}'
-                with open(q, 'wb') as file2:
-                    file2.write(response_image.content)
-
-                folder_for_comments = 'comments'
-                Path(folder_for_comments).mkdir(parents=True, exist_ok=True)
-                r = os.path.join(folder_for_comments, f'{name_file} - комментарий')
-                v = f'{r}.txt'
-                with open(v, 'w') as file:
-                    comments
-
-
-
-                filepath = download_txt(response_txt, name_file)
-                print(filepath)  # Выведется books/Алиби.txt
-
-                filepath2 = download_images(response_image, name_image)
-                print(filepath2)
+                download_txt(response_txt, f'{x+1}. {tittle}')
+                download_images(response_image, image)
+                download_comments(comments, f'{x+1}. {tittle} - комментарии')
 
 
 
@@ -161,6 +105,39 @@ def download_images(url, filename, folder='images/'):
     with open(filepath, 'wb') as file:
         file.write(url.content)
     return filepath
+
+def download_comments(comments, filename, folder='comments/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    filepath_without_format = os.path.join(folder, sanitize_filename(filename))
+    filepath = f'{filepath_without_format}.txt'
+    with open(filepath, 'w') : comments
+    return filepath
+
+def parse_book_page(soup):
+    # Заголовок
+    tittle2, author = soup.find('h1').text.split('::')
+    tittle = sanitize_filename(tittle2.strip())
+
+    # Комментарии
+    comments_tag = soup.find_all(class_='texts')
+    comments = []
+    for comment in comments_tag:
+        comments.append(comment.find(class_='black').text)
+
+    # Жанр
+    genre_tag = soup.find('span', class_='d_book').find_all('a')
+    genres = []
+    for genre in genre_tag:
+        genres.append(genre.text)
+
+    # Картинка
+    image_tag = soup.find(class_='bookimage').find('img')['src']
+    image = image_tag.split('/')[-1]
+
+
+    return tittle, author, comments, genres, image, image_tag
+
+
 
 f1 ()
 
